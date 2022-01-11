f1c62e4e017e78e1a79cb185c63d40bc5cd4f932
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-01-11 16:21:10 +0530

    Update Printing_numbers.java

--- Recursion/Printing_numbers.java
+++ Recursion/Printing_numbers.java
@@ -1,7 +1,7 @@
 class recursion{
     public static void printNumbers(int n){
         if(n==0){
-            return; //breaks the calls
+            return; //breaks the calls,exits the function
         }
         System.out.println(n); //print the number  5->4->3->2->1
         printNumbers(n-1);     //function calls itself for values from n-1 to 1.
