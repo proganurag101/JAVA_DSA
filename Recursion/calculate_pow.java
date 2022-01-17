ddbcb4328806cd6b3592a2b1489d49a1c7f31d2e
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-01-17 17:06:16 +0530

    Update calculate_pow.java

--- Recursion/calculate_pow.java
+++ Recursion/calculate_pow.java
@@ -1,7 +1,7 @@
 
 //calculate x^n;
 
-class first_proj{
+class Recursion{
     public static int calcPower(int x,int n){
         if(x==0){  // base case-1
             return 0;
@@ -23,3 +23,30 @@ class first_proj{
 Note:-
   (i)Here function call stack will be of height n+1.
   (ii)Now,we can reduce the stack height to log(n);
+
+class recursion{
+    public static int calcPower(int x,int n){
+        if(x==0){ // base case-1
+            return 0;
+        }
+        if(n==0){ //base case=2
+            return 1;
+        }
+        if(n%2==0){
+            return calcPower(x,n/2)*calcPower(x, n/2);
+        }else{
+            return calcPower(x, n/2)*calcPower(x, n/2)*x;
+        }
+    }
+    public static void main(String[] args) {
+        int x=2,n=5;
+        System.out.println(calcPower(x,n));
+
+
+    }
+}
+
+
+
+
+
