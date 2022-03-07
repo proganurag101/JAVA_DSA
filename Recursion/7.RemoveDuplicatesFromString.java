112d9ad9c796b4cf2fe00efd2223e48c0dbd6af4
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-03-07 20:51:31 +0530

    Update 7.RemoveDuplicatesFromString.java

--- Recursion/7.RemoveDuplicatesFromString.java
+++ Recursion/7.RemoveDuplicatesFromString.java
@@ -1,18 +1,18 @@
 
 
  class Main{
-    public static boolean [] map = new boolean [26];
+    public static boolean [] map = new boolean [26];        //the map is false(0) by default.
     public static void removeDuplicates(String str,int idx,String newStr){
        if(idx==str.length()){
-          System.out.println(newStr);
+          System.out.println(newStr);                  
           return;
        }
       char currChar = str.charAt(idx);
-       if(map[currChar-'a']){
-          removeDuplicates(str, idx+1,newStr);
+       if(map[currChar-'a']){                               //currChar-'a' -> gets the index of currChar
+          removeDuplicates(str, idx+1,newStr);             //if true we move on to the next index
        }else{
-          newStr += currChar;
-          map[currChar-'a']=true;
+          newStr += currChar;                              //if false we add the char to the newstring and set it to true
+          map[currChar-'a']=true;                           
           removeDuplicates(str, idx+1, newStr);
 
        }
