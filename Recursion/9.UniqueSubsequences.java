82963aad93502f05783066a4f39f13b4f6ac5f19
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-03-16 21:15:38 +0530

    Update 9.UniqueSubsequences.java

--- Recursion/9.UniqueSubsequences.java
+++ Recursion/9.UniqueSubsequences.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class main_oops{
     public static void subsequence(String str,int idx,String newStr,HashSet<String>set){
         if(idx==str.length()){
-            if(set.contains(newStr)){
-                return;
+            if(set.contains(newStr)){ //if contains then just return
+                return;                  
             }else{
-                System.out.println(newStr);
+                System.out.println(newStr); // if not then add it to the hashset and then return
                 set.add(newStr);
                 return;
             }
